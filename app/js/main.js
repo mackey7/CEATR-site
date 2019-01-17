6aaba35461ca48273976520f2eeeb64b3fa557e3
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-01-17 21:18:19 +0100

    first commit

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,46 @@
-console.log('This is the Main.js file. It should be the 3rd and final file');
+const slider = (function () {
+
+    let slideIndex = 1;
+    showSlides(slideIndex);
+
+    const plusSlides = (n) => {
+        showSlides(slideIndex += n);
+    }
+
+    const currentSlide = (n) => {
+        showSlides(slideIndex = n);
+    }
+
+    function showSlides(n) {
+        const slides = document.getElementsByClassName("mySlides"),
+            dots = document.getElementsByClassName("dot");
+
+        if (n > slides.length) { slideIndex = 1 }
+        if (n < 1) { slideIndex = slides.length }
+        for (let i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        for (let i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[slideIndex - 1].style.display = "flex";
+        dots[slideIndex - 1].className += " active";
+    }
+
+    const nextBtn = document.querySelector(".nextSlide");
+    const prevBtn = document.querySelector(".prevSlide");
+
+    nextBtn.addEventListener('click', () => {
+        plusSlides(1);
+    });
+
+    prevBtn.addEventListener('click', () => {
+        plusSlides(-1);
+    });
+
+    const dots = [...document.querySelectorAll('.dot')];
+    dots.forEach((box, i) => box.addEventListener('click', () => {
+        currentSlide(i + 1)
+    }));
+
+})()
